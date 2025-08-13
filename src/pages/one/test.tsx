2e3508409e5f563b4e5fd13b2f2ba1636ad4ea0a
suhyeon7557 <oheh41706808@gmail.com>
2025-08-13 15:37:48 +0900

    테스트 영역에 깃 테스트 메세지 추가

--- src/pages/one/test.tsx
+++ src/pages/one/test.tsx
@@ -7,6 +7,9 @@ const Test = () => {
             <div className='test'>
                 <h3>테스트 영역</h3>
                 <p>테스트 영역</p>
+                <div>
+                    <span>깃 테스트중입니다. 테스트 중 잘되고있음</span>
+                </div>
             </div>
         </main>
     );
