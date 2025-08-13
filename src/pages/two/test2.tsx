a909a825fa464de83902177f146ec754bb0d941f
이시아 <ylee9526@gmail.com>
2025-08-13 16:23:51 +0900

    테스트2 영역에 추가 메시지 및 설명 추가

--- src/pages/two/test2.tsx
+++ src/pages/two/test2.tsx
@@ -4,6 +4,10 @@ const Test2 = () => {
     return (
         <main>
             <h2 className='notext'>테스트2 영역</h2>
+            <div>
+                <h3>테스트2 영역</h3>
+                <p>이번엔 main_si에서 push 해보자 날찾아봐</p>
+            </div>
         </main>
     );
 }
