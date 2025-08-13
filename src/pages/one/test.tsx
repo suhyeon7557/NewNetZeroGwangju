01be76f8a6278db91f085c66a2a43d195a97e66c
suhyeon7557 <oheh41706808@gmail.com>
2025-08-13 18:04:49 +0900

    테스트 영역에 파란색 버튼 추가 및 스타일 정의

--- src/pages/one/test.tsx
+++ src/pages/one/test.tsx
@@ -3,7 +3,8 @@ import React from 'react';
 const Test = () => {
     return (
         <main>
-            <h2 className='notext'>테스트 영역</h2>
+			<h2 className='notext'>테스트 영역</h2>
+			<button className='Btn_blue' type='button'>테스트 버튼</button>
             <div className='test'>
                 <h3>테스트 영역</h3>
                 <p>마스터 테스트 영역 마스터 테스트 </p>
