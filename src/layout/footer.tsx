b1b6fa6ac4ce1cb1eb0a5e3fb7b2aa9f56b7b4fd
suhyeon7557 <oheh41706808@gmail.com>
2025-08-20 18:02:36 +0900

    푸터 및 헤더 컴포넌트 스타일 개선: 푸터 레이아웃 및 정보 섹션 추가, 헤더 메뉴 항목에 aria-expanded 속성 추가, 전역 스타일 조정으로 sticky footer 레이아웃 구현

--- src/layout/footer.tsx
+++ src/layout/footer.tsx
@@ -2,9 +2,36 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <footer>
-            <h2 className='notext'>푸터영역</h2>
-            <button type="button">보내기</button>
+        <footer id='footer'>
+            <h2 className='notext' style={{display: 'none'}}>푸터영역</h2>
+            <div className='footer_wrap'>
+                <div className='footbanner'>
+
+                </div>
+                <div className='section'>
+                    {/*logo*/}
+                    <div id='logo' className='black'>
+                        <a href='#' aria-label='홈'></a>
+                    </div>
+                    <div className='footer_info'>
+                        <p className='address'>(61954) 광주광역시 서구 천변우하로 181 TEL.062.601.1311 FAX.062.601.1313</p>
+                        <p className='copyright'>© 2023 Gwangju Climate & Energy Agency. All Rights Reserved.</p>
+                    </div>
+                    <div className='footer_sns'>
+                        <ul>
+                            <li>
+                                <a href='#' aria-label='페이스북'></a>
+                            </li>
+                            <li>
+                                <a href='#' aria-label='인스타그램'></a>
+                            </li>
+                            <li>
+                                <a href='#' aria-label='유튜브'></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
         </footer>
     );
 }
